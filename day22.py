8ea283c3a752572b72fb54f0a612f80894735276
xx1003 <wntnwls2002@gmail.com>
2024-02-14 14:25:08 +0900

    v1.2 dfs

--- day22.py
+++ day22.py
@@ -1,18 +1,26 @@
+# graph = [
+#     [0, 1, 0, 1, 0],
+#     [1, 0, 1, 1, 0],
+#     [0, 1, 0, 0, 1],
+#     [1, 1, 0, 0, 1],
+#     [0, 0, 1, 1, 0]
+# ]
+
 graph = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 1, 0],
-    [0, 1, 0, 0, 1],
-    [1, 1, 0, 0, 1],
-    [0, 0, 1, 1, 0]
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0]
 ]
 
 
-def dfs(g, v, visited):
-    visited[v] = True
-    print(chr(ord('A')+v), end=' ')
-    for i in range(len(g)):
-        if g[v][i] == True and not visited[i]:
-            dfs(g, i, visited)
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and visited[j] == 0:
+            dfs(g, j, visited)
 
 
 visited = [0] * len(graph)
