446338a5aad6ad08598fb4f3db66fee8c75d821c
xx1003 <wntnwls2002@gmail.com>
2024-02-14 16:55:22 +0900

    v1.6 fibonacci recursion

--- day22.py
+++ day22.py
@@ -1,16 +1,33 @@
-def decimal_to_octal(number:int) -> str:
+def fibo_recursion(number: int) -> int:
     """
-    decimal number to octal number (by recursion)
-    :param number: integer (base dec)
-    :return: string (base octal)
+    fibonacci function by recursion.
+    :param number: integer number
+    :return: integer number
     """
-
-    octal = ''
-    if number < 8:
-        return str(number)
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
     else:
-        return decimal_to_octal(number // 8) + str(number % 8)
+        return fibo_recursion(number - 1) + fibo_recursion(number - 2)
 
 
-n = int(input("Input decimal number : "))
-print(decimal_to_octal(n))
+def fibo_repetition(number):
+    a = 0
+    b = 1
+    for _ in range(number):
+        # a = b
+        # b = a + b
+        a, b = b, a + b
+    return a
+
+
+n = int(input("Input number : "))
+for i in range(0, n):
+    print(i)
+    print(fibo_recursion(i))
+
+for i in range(0, n):
+    print(i)
+    print(fibo_repetition(i))
+
