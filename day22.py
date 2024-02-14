f762fda37e5bcd43d6904a11507fd9e6e44e02b7
xx1003 <wntnwls2002@gmail.com>
2024-02-14 17:38:25 +0900

    v1.8 fibonacci memoization

--- day22.py
+++ day22.py
@@ -26,27 +26,29 @@ def fibo_repetition(number):
 memo = [None for _ in range(100)]
 
 
-def fibo_memoization(number: int, memo) -> int:
+def fibo_memoization(number: int) -> int:
     """
        fibonacci function by recursion with memoization.
        :param number: integer number
        :return: integer number
        """
+    global memo
     if memo[number] is not None:
         return memo[number]
     if number < 2:
+        memo[number] = number
         result = number
     else:
-        result = fibo_memoization(number-1, memo) + fibo_memoization(number-2, memo)
+        result = fibo_memoization(number-1) + fibo_memoization(number-2)
         memo[number] = result
     return result
 
 
 n = int(input("Input number : "))
-
+print(fibo_memoization(4), memo)
 for i in range(0, n):
     print(i)
-    print(fibo_memoization(i, memo))
+    print(fibo_memoization(i))
 print("================================")
 for i in range(0, n):
     print(i)
